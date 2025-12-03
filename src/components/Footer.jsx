c4da7d6c0026d27e2799bc4c63205931a3374357
Luis Enrique Galvan Morales <lusen121502@gmail.com>
2025-12-03 14:17:19 -0500

    Footer creado adsadsadsaskñmdasd

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-white text-center py-3 mt-5">
+      <p className="mb-0">© 2025 Examen - Examen que me salvara para no perder el año uwu</p>
+    </footer>
+  );
+};
+
+export default Footer;
